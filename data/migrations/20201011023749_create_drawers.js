00ccb39bb340d84a03af30d0b04ae76d85bdedc8
thejhubbs <thejhubbs@gmail.com>
2020-10-11 02:42:21 -0400

    D

---
+++ data/migrations/20201011023749_create_drawers.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('drawers', tbl => {
+        tbl.increments('drawer_id');
+
+        tbl.integer('order')
+
+        tbl.text('name')
+        tbl.text('class')
+        tbl.text('object_type')
+
+        tbl.specificType('objects', 'text ARRAY');
+        
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('drawers')
+  
+};
