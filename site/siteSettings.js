c085eed54fb5cfd8bd1ffef6300ae1f2c11540f6
thejhubbs <thejhubbs@gmail.com>
2020-11-27 20:01:24 -0500

    FP

--- site/siteSettings.js
+++ site/siteSettings.js
@@ -2,7 +2,7 @@ const siteName = "College Prep Science Interactive Lab"
 const siteEmail = ""
 const secretEmail = ""
 const siteLink = "https://interactivelabscience.netlify.com"
-//
+///////
 module.exports = {
     siteName,
     siteEmail,
