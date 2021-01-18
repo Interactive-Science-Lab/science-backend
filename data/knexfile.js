26c1234ac16ad5a6666ee2d439f4de27055f3c62
jordan hubbard <thejhubbs@gmail.com>
2021-01-18 16:29:40 -0500

    Debugging database

--- data/knexfile.js
+++ data/knexfile.js
@@ -30,8 +30,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: { directory: './migrations' },
-    seeds: { directory: './seeds' },
-    ssl: true
+    seeds: { directory: './seeds' }
   }
 
 
